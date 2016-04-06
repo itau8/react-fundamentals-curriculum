5e6f1b434c4400d9b72a0773b9a3ff01143c6508
itau-dev-o <itau@tuta.io>
2016-04-06 18:25:43 -0400

    curriculum started

---
+++ app/components/Main.js
@@ -0,0 +1,12 @@
+var React = require('react');
+
+
+var Main = React.createClass({
+	render:function () {
+		return (
+			<h1>HELLLO WORLD!!!</h1>
+		)
+	}
+});
+
+module.exports = Main;
---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+var htmlWebpackPlugin = require('html-webpack-plugin');
+var HtmlWebpackPluginConfig = new htmlWebpackPlugin({
+    template: __dirname + '/app/index.html',
+    filename: 'index.html',
+    inject: 'body'
+});
+
+module.exports = {
+
+    entry: [
+    './app/index.js'
+    ],
+    
+    output: {
+    path: __dirname + '/dist',
+    filename: 'index_bundle.js'
+    },
+
+    module: {
+
+        loaders: [
+
+            {test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader'},
+            {test:/\.css$/, loader:"style-loader!css-loader"}
+
+        ]
+    },
+
+    plugins: [HtmlWebpackPluginConfig]
+
+};
